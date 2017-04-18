742004a1d646a1c0dd9f81c109315e150ad87d50
Alex Filatov <alex@alexfilatov.com>
2017-04-18 12:39:09 +0300

    bumped ecto version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Translator.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps, 
+     deps: deps,
 
      # Hex
      description: description,
@@ -34,7 +34,7 @@ defmodule Translator.Mixfile do
   end
 
   defp deps do
-    [{:ecto, "~> 2.0.0-rc"},
+    [{:ecto, "~> 2.1.4"},
      {:ex_doc, "~> 0.11", only: :docs}]
   end
 end
